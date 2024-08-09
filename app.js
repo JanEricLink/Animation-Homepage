fff55d93d8b01375fafddaf5c2bbfe196d015242
JanEricLink <145751242+JanEricLink@users.noreply.github.com>
2024-08-09 12:54:17 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
 function changeNightMode() {
-      let nMode = false;
-      let root = document.styleSheets[1].cssRules[0];
-        if (nMode) {
-          root.style.setProperty("--backgroundcolor", "rgb(240,240,240)");
-          root.style.setProperty("--textcolor", "rgb(63,63,63)");
-          root.style.setProperty("--primary", " rgba(255,235,60,0.6)");
-          document.getElementById("nModeButton").innerHTML = "🌙";
-          nMode = false;
-        } else {
-          root.style.setProperty("--backgroundcolor", "rgb(63,63,63)");
-          root.style.setProperty("--textcolor", "rgb(240,240,240)");
-          root.style.setProperty("--primary", "rgba(60,235,255,0.6");
-          document.getElementById("nModeButton").innerHTML = "☀️";
-          nMode = true;
-        }
-      }
+  let nMode = false;
+  let root = document.styleSheets[1].cssRules[0];
+  if (nMode) {
+    root.style.setProperty("--backgroundcolor", "rgb(240,240,240)");
+    root.style.setProperty("--textcolor", "rgb(63,63,63)");
+    root.style.setProperty("--primary", " rgba(255,235,60,0.6)");
+    document.getElementById("nModeButton").innerHTML = "🌙";
+    nMode = false;
+  } else {
+    root.style.setProperty("--backgroundcolor", "rgb(63,63,63)");
+    root.style.setProperty("--textcolor", "rgb(240,240,240)");
+    root.style.setProperty("--primary", "rgba(60,235,255,0.6");
+    document.getElementById("nModeButton").innerHTML = "☀️";
+    nMode = true;
+  }
+}
 
-      function changeAnimationToTrig() {
-        let iframe = document.getElementById("animationFrame");
-        iframe.src = "https://janericlink.github.io/Trig-Animation/";
-        iframe.title = "Animation of the Trigonometric Functions";
-      }
+function changeAnimationToTrig() {
+  let iframe = document.getElementById("animationFrame");
+  iframe.src = "https://janericlink.github.io/Trig-Animation/";
+  iframe.title = "Animation of the Trigonometric Functions";
+}
 
-      function changeAnimationToWater() {
-        let iframe = document.getElementById("animationFrame");
-        iframe.src = "https://janericlink.github.io/Water-Wave-Animation/";
-        iframe.title = "Animation of a Water Wave";
-      }
+function changeAnimationToWater() {
+  let iframe = document.getElementById("animationFrame");
+  iframe.src = "https://janericlink.github.io/Water-Wave-Animation/";
+  iframe.title = "Animation of a Water Wave";
+}
 
-      function changeAnimationToSound() {
-        let iframe = document.getElementById("animationFrame");
-        iframe.src = "https://janericlink.github.io/Sound-Wave-Animation/";
-        iframe.title = "Animation of a Sound Wave";
-      }
+function changeAnimationToSound() {
+  let iframe = document.getElementById("animationFrame");
+  iframe.src = "https://janericlink.github.io/Sound-Wave-Animation/";
+  iframe.title = "Animation of a Sound Wave";
+}
